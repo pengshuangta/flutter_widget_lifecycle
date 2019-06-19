2005078bdd574b776ed9441ebd042b1827e79efd
彭双塔 <arthur@soundbus.cn>
2019-06-19 10:07:55 +0800

    1、创建项目

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '生命周期',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        brightness: Brightness.light,
+      ),
+      home: LifecycleAppPage(),
+    );
+  }
+}
+
+class LifecycleAppPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new _LifecycleAppPageState('构造函数');
+  }
+}
+
+class _LifecycleAppPageState extends State<LifecycleAppPage>
+    with WidgetsBindingObserver {
+  String str;
+
+  int count = 0;
+
+  _LifecycleAppPageState(this.str);
+
+  @override
+  void initState() {
+    print(str);
+    print('initState');
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(LifecycleAppPage oldWidget) {
+    print('didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    print('deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('dispose');
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.inactive:
+        print('AppLifecycleState.inactive');
+        break;
+      case AppLifecycleState.paused:
+        print('AppLifecycleState.paused');
+        break;
+      case AppLifecycleState.resumed:
+        print('AppLifecycleState.resumed');
+        break;
+      case AppLifecycleState.suspending:
+        print('AppLifecycleState.suspending');
+        break;
+    }
+
+    super.didChangeAppLifecycleState(state);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('lifecycle 学习'),
+        centerTitle: true,
+      ),
+      body: new OrientationBuilder(
+        builder: (context, orientation) {
+          return new Center(
+            child: new Text(
+              '当前计数值：$count',
+              style: new TextStyle(
+                  color: orientation == Orientation.portrait
+                      ? Colors.blue
+                      : Colors.red),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: new FloatingActionButton(
+          child: new Text('click'),
+          onPressed: () {
+            count++;
+            setState(() {});
+          }),
+    );
+  }
+}
+
+class LifecyclePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new LifecycleAppPage(),
+    );
+  }
+}
